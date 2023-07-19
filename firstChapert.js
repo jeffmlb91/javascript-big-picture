1d79af5234769ba92d6bcf345211e7e27c56def8
jeffmlb91 <jeffmlb91@gmail.com>
2023-07-18 23:05:43 -0400

    initial commit

---
+++ firstChapert.js
@@ -0,0 +1,27 @@
+{/**
+You order a t-shirt for $10, socks for $8 and dinner plates $20
+use Javascript to calculate the total cost of your order
+*/}
+const tShirtPrice = 10;
+const socks = 8;
+const dinnerPlate = 20;
+
+const totalCost = tShirtPrice + socks + dinnerPlate;
+console.log(`the total amount of my spending is $` ,`${totalCost}`);
+
+{/**
+Your bank account has $100, you spend $20 on lunch, $50 on dinner and $
+and earn $200 from your job. Calculate how much money you have
+*/}
+
+const totalAmountInBankAccount = 100;
+const lunchPrice = 20;
+const dinnerPrice = 50;
+const salaryEarned = 200;
+
+const availableMoney = totalAmountInBankAccount - lunchPrice - dinnerPrice + salaryEarned
+console.log(`I currentlt have $ ${availableMoney} in my account`)
+{/**
+use document.body.innerHtml =....; to make a web page blank
+*/}
+
